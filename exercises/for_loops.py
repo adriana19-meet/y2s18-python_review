628174abf3f5519ff22c3577e290d6edf1bd16ad
aboud20-meet <aboud20@meet.mit.edu>
2018-08-05 16:30:34 +0300

    python

--- exercises/for_loops.py
+++ exercises/for_loops.py
@@ -1,1 +1,7 @@
 # Write your solution for 1.1 here!
+sum = 0
+for i in range(1,101):
+	print (i) 
+	sum=sum+i
+print (sum)
+
--- exercises/functions.py
+++ exercises/functions.py
@@ -1,4 +1,10 @@
 # Write your solution for 1.4 here!
 
+i = 2
 def is_prime(x):
+	while(i<x):
+       print("not prime")
+
+  if(x % i==0):
+			print("prime")
 	pass
